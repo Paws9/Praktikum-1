e68f91044960c31b68d76b5f7c2e5e78a76e65b4
Paws9 <zakyfawwaz9@gmail.com>
2022-09-21 10:53:39 +0700

    Create Praktikum algoritma 2.py

---
+++ Praktikum algoritma 2.py
@@ -0,0 +1,6 @@
+print("Menghituang Luas Ruangan")
+s = str(input("Pilih salah satu satuan yang digunakan Meter/Inchi? "))
+pr = float(input("Berapa Panjang Ruangan Anda? "))
+lb = float(input("Berapa Lebar Ruangan Anda?  "))
+luas= pr*lb
+print("Maka Luas Ruangan Anda Adalah: ",float(luas),(s))
