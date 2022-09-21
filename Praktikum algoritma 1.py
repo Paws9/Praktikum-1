f7a30f684f335c99b2c66e9d3ef45650d62e117d
Paws9 <zakyfawwaz9@gmail.com>
2022-09-21 10:46:38 +0700

    Create Praktikum algoritma 1.py

---
+++ Praktikum algoritma 1.py
@@ -0,0 +1,7 @@
+nama = input("Nama namu siapa? : ")
+asal = input("Dari asal kamu? : ")
+sekolah = input("sebelumnya bersekolah dimana?: ")
+
+print("cool name",nama)
+print("ohhh kamu dari",asal)
+print("ohh jadi kamu dari",sekolah)
